299b3b6726bf576fa88592eca22a0ca7d81c7049
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-06-28 07:38:43 -0700

    update starter code

--- lib/ruby_fundamentals.rb
+++ lib/ruby_fundamentals.rb
@@ -1,6 +1,7 @@
-require "ruby_fundamentals/version"
-require "ruby_fundamentals/friends"
-require "ruby_fundamentals/accessing_data"
+require_relative "ruby_fundamentals/version"
+require_relative "ruby_fundamentals/friends"
+require_relative "ruby_fundamentals/accessing_data"
+require_relative "ruby_fundamentals/playground"
 
 module RubyFundamentals
   class Error < StandardError; end
